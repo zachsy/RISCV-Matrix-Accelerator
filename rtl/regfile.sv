e59f5c1163433eaa60b199fffcd258413f8e8490
Zachary Liu <zachsyliu@gmail.com>
2026-07-21 14:15:10 -0700

    Added Read and Write to the register.

--- rtl/regfile.sv
+++ rtl/regfile.sv
@@ -11,7 +11,14 @@ module regfile (
 
     logic [31:0] rf [31:0];
     
-    //TODO: Combinational Reads (Outputs rd1 and rd2)
+    assign rd1 = (a1 == 0) ? 32'b0 : rf[a1];
+    assign rd2 = (a2 == 0) ? 32'b0 : rf[a2];
+
+    always_ff @(posedge clk) begin
+        if (we3 == 1) begin
+            rf[a3] <= wd3;
+        end
+    end
     //TODO: Synchronous Write (Writing wd3 into a3)
 
 endmodule
