ce10d512804f02d514ebdfd00e85aebf3220a77f
Zachary Liu <zachsyliu@gmail.com>
2026-08-28 23:20:50 -0700

    feat(scripts): Parameterize matrix test generator.
    Make the test generator scale to any data width and array size
    
    - change sum_W so that it scales with data_W rather than needing to be changed manually
    - add a fileWrite function to write values into the files in hex. This function works for any data width.
    - split a = w = c initialization into independent arrays
    - paramaterize random test values as -(2**(data_W-1)), (2**(data_W-1)) instead of beign hardcoded

--- scripts/generate_accelerator_tests.py
+++ scripts/generate_accelerator_tests.py
@@ -1,64 +1,59 @@
 import numpy as np
 
 # KNOBS
-arraySize = 4       # Changed depending on size of array. Should match 'int N' in systolic_array.sv and tb_systolic_array.sv
-num_tests = 1000    # Number of random tests not including edge case testing.
-data_W    = 8       # Bit width of A and W elements (e.g. INT4=4, INT8=8, INT16=16)
-sum_W     = 32      # Bit width of accumulated C elements (must be >= 2*DATA_W + ceil(log2(N)))
+arraySize = 4                                       # Changed depending on size of array. Should match 'int N' in systolic_array.sv and tb_systolic_array.sv
+num_tests = 1000                                    # Number of random tests not including edge case testing.
+data_W    = 8                                       # Bit width of A and W elements (e.g. INT4=4, INT8=8, INT16=16)
+sum_W = 2*data_W + (arraySize - 1).bit_length()     # Bit width of accumulated C elements (must be >= 2*DATA_W + ceil(log2(N)))
+
+def fileWrite(mat, file, bitWidth):
+    for val in mat.flatten():
+        hexWidth = (bitWidth + 3) // 4
+        mask = (1 << bitWidth) - 1
+        file.write(f"{val & mask:0{hexWidth}x} ")
+    file.write("\n")
+    return
 
 with open("sim/A.tv", "w") as fa, open("sim/W.tv", "w") as fw, open("sim/C.tv", "w") as fc:
     total_tests = 0
 
     # Identity Matrix:
-    a = w = c = np.identity(arraySize, dtype = int)
-    for val in a.flatten():
-        fa.write(f"{val & 0xFF:02x} ")
-    fa.write("\n")
-    for val in w.flatten():
-        fw.write(f"{val & 0xFF:02x} ")
-    fw.write("\n")
-    for val in c.flatten():
-        fc.write(f"{val & 0xFFFFFFFF:08x} ")
-    fc.write("\n")
+    a = np.identity(arraySize, dtype = int)
+    w = np.identity(arraySize, dtype = int)
+    c = np.identity(arraySize, dtype = int)
+
+    fileWrite(a, fa, data_W)
+    fileWrite(w, fw, data_W)
+    fileWrite(c, fc, sum_W)
+
     total_tests += 1
 
 
     # Zero Matrix, One Matrix, Min and Max Matrix: 
-    test_values = [-128, 0, 1, 127]
+    test_values = [-(2**(data_W-1)), 0, 1, ((2**(data_W-1)) - 1)]
 
     for test_val in test_values:
-        a = w = np.full((arraySize, arraySize), test_val)
-
+        a = np.full((arraySize, arraySize), test_val)
+        w = np.full((arraySize, arraySize), test_val)
         c = a @ w
 
-        for val in a.flatten():
-            fa.write(f"{val & 0xFF:02x} ")
-        fa.write("\n")
-        for val in w.flatten():
-            fw.write(f"{val & 0xFF:02x} ")
-        fw.write("\n")
-        for val in c.flatten():
-            fc.write(f"{val & 0xFFFFFFFF:08x} ")
-        fc.write("\n")
+        fileWrite(a, fa, data_W)
+        fileWrite(w, fw, data_W)
+        fileWrite(c, fc, sum_W)
+
         total_tests += 1
     
 
     # Random Testing:     
     for _ in range(num_tests):
-        a = np.random.randint(-128, 128, (arraySize, arraySize))
-        w = np.random.randint(-128, 128, (arraySize, arraySize))
+        a = np.random.randint(-(2**(data_W-1)), (2**(data_W-1)), (arraySize, arraySize))
+        w = np.random.randint(-(2**(data_W-1)), (2**(data_W-1)), (arraySize, arraySize))
 
         c = a @ w
 
-        for val in a.flatten():
-            fa.write(f"{val & 0xFF:02x} ")
-        fa.write("\n")
-        for val in w.flatten():
-            fw.write(f"{val & 0xFF:02x} ")
-        fw.write("\n")
-        for val in c.flatten():
-            fc.write(f"{val & 0xFFFFFFFF:08x} ")
-        fc.write("\n")
+        fileWrite(a, fa, data_W)
+        fileWrite(w, fw, data_W)
+        fileWrite(c, fc, sum_W)
 
         total_tests += 1
 
