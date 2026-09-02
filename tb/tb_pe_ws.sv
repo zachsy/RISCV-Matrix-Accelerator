27deba8bc9612be292fcf725c38cd0055e0d0b79
Zachary Liu <zachsyliu@gmail.com>
2026-09-02 13:37:01 -0700

    chore(pe): rename pe test file to tb_pe_ws.sv to reflect testbench purpose for the weight-stationary PE only

---
+++ tb/tb_pe_ws.sv
@@ -0,0 +1,45 @@
+module tb_pe_ws();
+    logic        clk, reset, load_weight;
+    logic [7:0]  a_in, weight_in;
+    logic [31:0] sum_in;
+    logic [7:0]  a_out;
+    logic [31:0] sum_out;
+
+    pe_ws dut (
+        .clk(clk),
+        .reset(reset),
+        .load_weight(load_weight),
+        .a_in(a_in),
+        .sum_in(sum_in),
+        .weight_in(weight_in),
+        .a_out(a_out),
+        .sum_out(sum_out)
+    );
+
+    // Clock
+    always begin
+        clk = 1; #5; clk = 0; #5;
+    end
+
+    initial begin
+        $dumpfile("sim/pe_ws_waves.vcd");
+        $dumpvars(0, tb_pe_ws);
+
+        reset = 1; load_weight = 0;
+        a_in = 0; weight_in = 0; sum_in = 0;
+
+        #20; reset = 0;
+
+        #10;
+        load_weight = 1;
+        weight_in = 8'd23;
+
+        #10;
+        load_weight = 0;
+        a_in = 8'd12;
+        sum_in = 8'd200;
+
+        #20;
+        $finish;
+    end
+endmodule
