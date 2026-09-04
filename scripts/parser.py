12afffd3662da4716b089b01d15707e91abd1a50
Zachary Liu <zachsyliu@gmail.com>
2026-09-04 09:46:40 -0700

    feat(parser): Finish parser.py along with first result in results folder
    Parser.py runs a opens util.rpt and timing.rpt and extracts LUTs, FFs, BRAM, DSP, and WNS. Then, takes in inputs of corresponding N, Data W, Sum W, and Dataflow values and records on a .csv file stored in results/results.csv.

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,3 +1,7 @@
+import csv
+import os
+import argparse
+
 PERIOD_NS   = 6.667
 lut         = None
 flip_flop   = None
@@ -29,3 +33,26 @@ with open("build/timing.rpt", "r") as timing:
             wns = float(parts[7].strip().rstrip("ns,"))   # '-0.531ns,' -> -0.531
             fmax_mhz  = 1000.0 / (PERIOD_NS - wns)
             break
+
+
+p = argparse.ArgumentParser()
+p.add_argument("--n",           type=int, default=4)
+p.add_argument("--data_w",      type=int, default=8)
+p.add_argument("--sum_w",       type=int, default=18)
+p.add_argument("--dataflow",    type=str, default='ws')
+args = p.parse_args()
+
+n   = args.n
+data_W      = args.data_w
+sum_W       = args.sum_w
+dataflow    = args.dataflow
+
+data_order = [n, dataflow, data_W, sum_W, lut, flip_flop, bram, dsp, fmax_mhz]
+
+with open('results/results.csv', 'a', newline='') as r:
+    writer = csv.writer(r)
+
+    if os.stat('results/results.csv').st_size == 0:
+        writer.writerow(['Array Size', 'Dataflow', 'Data Width', 'Sum Width', 'LUTs', 'FFs', 'BRAM', 'DSP', 'Fmax (MHz)'])
+
+    writer.writerow(data_order)
